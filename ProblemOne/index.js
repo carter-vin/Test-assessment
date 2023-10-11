2ebacaebd006cd45869b8803eec5c7a9a564dcc9
carter-vin <cartervincent684@gmail.com>
2023-10-12 02:02:03 +0530

    - added functionality to do flatten the array

--- ProblemOne/index.js
+++ ProblemOne/index.js
@@ -4,8 +4,8 @@ var arr = [
     'first_name': 'Marco',
     'last_name': 'Burns',
     'guest_booking': {
-        'room_no': 'A0073',
-        'some_array': [7,2,4]
+      'room_no': 'A0073',
+      'some_array': [7, 2, 4]
     },
   },
   {
@@ -13,8 +13,8 @@ var arr = [
     'first_name': 'John',
     'last_name': 'Doe',
     'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
     },
   },
   {
@@ -22,8 +22,8 @@ var arr = [
     'first_name': 'Jane',
     'last_name': 'Doe',
     'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
     },
   },
   {
@@ -31,8 +31,8 @@ var arr = [
     'first_name': 'Albert',
     'last_name': 'Einstein',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
   {
@@ -40,8 +40,8 @@ var arr = [
     'first_name': 'Jack',
     'last_name': 'Daniels',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
   {
@@ -49,17 +49,36 @@ var arr = [
     'first_name': 'Alan',
     'last_name': 'Turing',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
 ];
 
-function mutateArray(a) {
-    return a;
+function doFlatten(item, result = {}) {
+  for (const key in item) {
+    if (Object.hasOwnProperty.call(item, key)) {
+      const value = item[key];
+      if (Array.isArray(value)) {
+        result[key] = value
+      } else if (value !== null && typeof value === 'object') {
+        doFlatten(value, result)
+      } else {
+        result[key] = value
+      }
+    }
+  }
+  return result
 }
 
-$(document).ready(function() {
-    $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+function mutateArray(a = []) {
+  // 1. Update the `mutateArray` function to return `a` as a flattened array, so that each item is changed to
+  const flattenedArray = a.map(i => doFlatten(i));
+
+  return flattenedArray;
+}
+
+$(document).ready(function () {
+  $('#originalArray').html(JSON.stringify(arr, null, 2));
+  $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
 });
